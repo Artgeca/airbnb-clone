c387b9f475898259bd35d1e7cd159b1436ee757e
Arturas Gecas <artgeca@gmail.com>
2023-07-10 10:47:28 +0300

    feat: github and google auth

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-hook-form';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
@@ -86,13 +87,13 @@ const RegisterModal = () => {
         outline
         label='Continue with Google'
         icon={FcGoogle}
-        onClick={() => { }}
+        onClick={() => signIn('google')}
       />
       <Button
         outline
         label='Continue with Github'
         icon={AiFillGithub}
-        onClick={() => { }}
+        onClick={() => signIn('github')}
       />
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='flex flex-row justify-center items-center gap-2'>
