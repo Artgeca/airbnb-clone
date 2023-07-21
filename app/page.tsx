4a0275489da24cb7e6b63f2474971fa04ce10552
artgeca <artgeca@gmail.com>
2023-07-21 19:08:52 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/first */
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
